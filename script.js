9da2a922ea78d411405127e67fe84842218787bb
ProM113K <putrad322@gmail.com>
2021-10-30 00:06:44 +0700

    third commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let lovePic = document.getElementById('love');
+let contKedua = document.getElementsByClassName("kedua");
 
 lovePic.addEventListener("click", function () {
     lovePic.style.display = "none";
-    
-})
+    // document.getElementsByTagName("div")[0].setAttribute("hidden", "hidden");
+    document.getElementById("p-1st").removeAttribute("hidden")
+});
