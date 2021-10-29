0e4a8e6942ca1b0614b8a4178e83e903951bf404
ProM113K <putrad322@gmail.com>
2021-10-30 01:03:53 +0700

    fifth commit

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let lovePic = document.getElementById('love');
 let contKedua = document.getElementsByClassName("kedua");
+let btnMenu = document.getElementById("menu");
 
 lovePic.addEventListener("click", function () {
     lovePic.style.display = "none";
     // document.getElementsByTagName("div")[0].setAttribute("hidden", "hidden");
-    document.getElementById("p-1st").removeAttribute("hidden")
+    document.getElementsByTagName("div")[1].removeAttribute("hidden")
 });
+
+btnMenu.addEventListener("click", function() {
+    btnMenu.setAttribute("hidden", "hidden");
+    document.getElementsByTagName("div")[0].removeAttribute("hidden");
+})
