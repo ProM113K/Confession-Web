a235dc7949473e937e13f997914a719d68445b4c
ProM113K <putrad322@gmail.com>
2021-10-30 05:17:16 +0700

    sixth commit

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 let lovePic = document.getElementById('love');
-let contKedua = document.getElementsByClassName("kedua");
+let contKedua = document.querySelector(".kedua");
 let btnMenu = document.getElementById("menu");
+let main = document.getElementById("main");
+let contPertama = document.querySelector(".pertama");
+let contAwal = document.querySelector(".awal");
 
 lovePic.addEventListener("click", function () {
     lovePic.style.display = "none";
-    // document.getElementsByTagName("div")[0].setAttribute("hidden", "hidden");
-    document.getElementsByTagName("div")[1].removeAttribute("hidden")
+    document.getElementsByTagName("div")[1].removeAttribute("hidden");
+    document.getElementById("main").appendChild(contAwal);
 });
 
 btnMenu.addEventListener("click", function() {
     btnMenu.setAttribute("hidden", "hidden");
     document.getElementsByTagName("div")[0].removeAttribute("hidden");
+    document.getElementsByTagName("section")[0].remove();
 })
