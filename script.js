979d48f582d86fd5838385a591a1bf9d083fc426
ProM113K <putrad322@gmail.com>
2021-10-31 03:13:29 +0700

    latest commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let lovePic = document.getElementById('love');
+let treePic = document.getElementById('tree');
 let contKedua = document.querySelector(".kedua");
 let btnMenu = document.getElementById("menu");
 let main = document.getElementById("main");
@@ -9,10 +10,17 @@ lovePic.addEventListener("click", function () {
     lovePic.style.display = "none";
     document.getElementsByTagName("div")[1].removeAttribute("hidden");
     document.getElementById("main").appendChild(contAwal);
-    contAwal.setAttribute("hidden","hidden");
+    contAwal.setAttribute("hidden", "hidden");
 });
 
-btnMenu.addEventListener("click", function() {
+treePic.addEventListener("click", function () {
+    treePic.style.display = "none";
+    document.getElementsByTagName("div")[2].removeAttribute("hidden");
+    document.getElementById("main").appendChild(contKedua);
+    contAwal.setAttribute("hidden", "hidden");
+});
+
+btnMenu.addEventListener("click", function () {
     btnMenu.setAttribute("hidden", "hidden");
     document.getElementsByTagName("div")[0].removeAttribute("hidden");
     document.getElementsByTagName("section")[0].remove();
